43998826ee9f6f56f7bbddb2d83e698333b6c49a
Johnnylaw182 <jonathan.jimenez34@gmail.com>
2019-01-15 15:15:24 -0800

    made car stuff

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var car = {
+		make: 'vw',
+		type: 'polo',
+		color: 'blue',
+		isTurnedOn: false,
+		numberOfWheels: 4,
+		Seats: [
+			'seat 1',
+			'seat 2',
+			'seat 3',
+			'seat 4',
+		],
+		turnOn: function() {
+			this.isTurnedOn = true;
+		},
+		fly: function () {
+				alert('fly');
+		},
+		switchCar: function (isOn) {
+			console.log('turn car'+isOn)
+				;
+			if (isOn == true) {
+				this.isTurnedOn = true;
+			} else {
+				this.isTurnedOn = false;
+			}
+		}
+
+	};	
+
+	console.log('hello js rocket');
